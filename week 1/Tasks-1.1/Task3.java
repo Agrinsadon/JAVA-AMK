d45771424a9dda75d589469ef3826822351308bd
Agrin <agrins@metropolia.fi>
2023-08-27 00:37:43 +0300

    DONE

---
+++ week 1/Tasks-1.1/Task3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+
+        float avg = (first + second + third);
+
+        System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("Number of the product is " + (first * second * third));
+        System.out.println("The average number is " + ((avg) / 3));
+    }
+}
