7d68d51c67384c215db7fbfe60e0c901aebcdc25
Agrin <agrins@metropolia.fi>
2023-08-27 00:15:19 +0300

    DONE

---
+++ week 1/Tasks-1.1/Task1.java
@@ -0,0 +1,6 @@
+public class Task1 {
+    public static void main(String[] args) {
+        String greeting = "Hello Agrin! Welcome back to school.";
+        System.out.println(greeting);
+    }
+}
