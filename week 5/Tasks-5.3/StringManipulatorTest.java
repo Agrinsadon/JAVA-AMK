17e09e65e8291c3b7edb5601c0e6e107695f0837
Agrin <agrins@metropolia.fi>
2023-12-04 15:25:27 +0200

    Week 5.3 Done made little adjustments

--- week 5/Tasks-5.3/StringManipulatorTest.java
+++ week 5/Tasks-5.3/StringManipulatorTest.java
@@ -1,58 +1,37 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-// You can test the code one by one i dont know if it is what is wanted on the task but it works
-class StringManipulator {
 
-    public String concatenate(String str1, String str2) {
-        return str1 + str2;
+public class StringManipulatorTest {
+
+    @Test
+    public void testConcatenate() {
+        StringManipulator stringManipulator = new StringManipulator();
+        assertEquals("HelloWorld", stringManipulator.concatenate("Hello", "World"));
     }
 
-    public int findLength(String str) {
-        return str.length();
+    @Test
+    public void testFindLength() {
+        StringManipulator stringManipulator = new StringManipulator();
+        assertEquals(5, stringManipulator.findLength("Hello"));
     }
 
-    public String convertToUpperCase(String str) {
-        return str.toUpperCase();
+    @Test
+    public void testConvertToUpperCase() {
+        StringManipulator stringManipulator = new StringManipulator();
+        assertEquals("HELLO", stringManipulator.convertToUpperCase("Hello"));
     }
 
-    public String convertToLowerCase(String str) {
-        return str.toLowerCase();
+    @Test
+    public void testConvertToLowerCase() {
+        StringManipulator stringManipulator = new StringManipulator();
+        assertEquals("hello", stringManipulator.convertToLowerCase("Hello"));
     }
 
-    public boolean containsSubstring(String str, String subStr) {
-        return str.contains(subStr);
-    }
-
-    public static class StringManipulatorTest {
-
-        @org.junit.Test
-        public void testConcatenate() {
-            StringManipulator stringManipulator = new StringManipulator();
-            assertEquals("HelloWorld", stringManipulator.concatenate("Hello", "World"));
-        }
-
-        @org.junit.Test
-        public void testFindLength() {
-            StringManipulator stringManipulator = new StringManipulator();
-            assertEquals(5, stringManipulator.findLength("Hello"));
-        }
-
-        @org.junit.Test
-        public void testConvertToUpperCase() {
-            StringManipulator stringManipulator = new StringManipulator();
-            assertEquals("HELLO", stringManipulator.convertToUpperCase("Hello"));
-        }
-
-        @org.junit.Test
-        public void testConvertToLowerCase() {
-            StringManipulator stringManipulator = new StringManipulator();
-            assertEquals("hello", stringManipulator.convertToLowerCase("Hello"));
-        }
-
-        @org.junit.Test
-        public void testContainsSubstring() {
-            StringManipulator stringManipulator = new StringManipulator();
-            assertTrue(stringManipulator.containsSubstring("Hello", "el"));
-            assertFalse(stringManipulator.containsSubstring("Hello", "abc"));
-        }
+    @Test
+    public void testContainsSubstring() {
+        StringManipulator stringManipulator = new StringManipulator();
+        assertTrue(stringManipulator.containsSubstring("Hello", "el"));
+        assertFalse(stringManipulator.containsSubstring("Hello", "abc"));
     }
 }
